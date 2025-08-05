d399f296783fa2681899ceb305d97889204ea1e6
campioru <campioru@tcd.ie>
2025-08-05 20:07:55 +0100

    variance re-write
    Similar re-write as in the previous commit, but for one of the variance reduction files. Added comments regarding fixing the code for a fair comparison, including errorbars in plots, and removing redundant code via loops.

--- Variance reduction/(1+X)^(-1)/(1+X)^(-1).py
+++ Variance reduction/(1+X)^(-1)/(1+X)^(-1).py
@@ -11,125 +11,74 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def mean(array):
-    """Calculate the mean of an array."""
-    total = 0.
-    for i in array:
-        total += i
-    return total / len(array)
-
-
-def average_list(array):
-    """Calculate the evolution of the mean of an array."""
-    result = np.zeros(len(array))
-    result[0] = array[0]
-    for i in range(1, len(array)):
-        result[i] = ((result[i-1] * i) + array[i]) / (i+1.)
-    return result
-
-
-def variance(array):
-    """Calculate the variance of an array."""
-    mean_ = mean(array)
-    total = 0.
-    for i in range(len(array)):
-        total += (array[i] - mean_) ** 2.
-    return total / (len(array) - 1.)
-
-
 K = 20
 n = 2 ** K
-q = np.log(2.)
-factors = np.empty(K)
-for k in range(K):
-    factors[k] = 2 ** (k+1)
 
-(average_ds, average_as, average_cs) = (np.empty(K) for _ in range(3))
-(xs, xs_) = (np.empty(n) for _ in range(2))
-for i in range(int(n/2)):
-    u = np.random.uniform(0, 1)
-    xs[2*i] = u
-    xs[2*i + 1] = np.random.uniform(0, 1)
-    xs_[2*i] = u
-    xs_[2*i + 1] = 1. - u
-d_s = (1. + xs) ** (-1.)
-a_s = (1. + xs_) ** (-1.)
+xs = np.random.uniform(0, 1, n)
+xs_ = xs.copy()
+xs_[1::2] = 1. - xs[::2]
+d_s = 1. / (1. + xs)
+a_s = 1. / (1. + xs_)
 
 mu = 1.5
-var = 2. * (.5 ** 3.) / 3.
-cov = mean((1. + xs) * d_s) - mu * mean(d_s)
+var = 1./3. - 1./(2.**2.)
+# TODO change to sample covariance at to each point
+# otherwise comparing method using all the data to methods involving a subset of the data
+cov = np.mean((1. + xs) * d_s) - mu * np.mean(d_s)
 cstar = -cov / var
 c_s = d_s + cstar * ((1. + xs) - mu)
 
-average_ds1 = average_list(d_s)
-average_as1 = average_list(a_s)
-average_cs1 = average_list(c_s)
-for k in range(K):
-    average_ds[k] = average_ds1[2**(k+1)-1]
-    average_as[k] = average_as1[2**(k+1)-1]
-    average_cs[k] = average_cs1[2**(k+1)-1]
+points = np.arange(2, n+1, 2)
+average_ds = np.cumsum(d_s)[1::2] / points
+average_as = np.cumsum(a_s)[1::2] / points
+average_cs = np.cumsum(c_s)[1::2] / points
 
-plt.axhline(y=q, color="k", linestyle="dashed")
-plt.plot(factors, average_ds, color="r", label="Direct")
-plt.plot(factors, average_as, color="g", label="Antithetic")
-plt.plot(factors, average_cs, color="b", label="Control")
+# TODO include errorbars (include autocorrelation)
+plt.axhline(y=np.log(2.), color="k", linestyle="dashed")
+plt.plot(points, average_ds, color="r", label="Direct")
+plt.plot(points, average_as, color="g", label="Antithetic")
+plt.plot(points, average_cs, color="b", label="Control")
 plt.xscale("log", base=2)
-plt.xlim(2., n)
+plt.xlim(2, n)
 plt.xlabel(r"$n$")
 plt.ylabel(r"$\bar{f}$")
 plt.title(r"$\int_0^1 \frac{1}{1+x}\,dx$")
 plt.legend()
 plt.savefig("(1+X1)^(-1).pdf", bbox_inches='tight')
 plt.clf()
+# TODO consider combining antithetic + control
 
 
+# TODO combine below into loop with above
 K = 20
 n = 2 ** K
-factors = np.empty(K)
-for k in range(K):
-    factors[k] = 2 ** (k+1)
 q = 10
 
-(average_ds, average_as, average_cs) = (np.empty(K) for _ in range(3))
-(xs, xs_) = (np.random.uniform(0, 1, (q, n)) for _ in range(2))
-for i in range(int(n/2)):
-    u = np.random.uniform(0, 1, q)
-    for j in range(q):
-        xs[j, 2*i] = u[j]
-        xs[j, 2*i + 1] = np.random.uniform(0, 1)
-        xs_[j, 2*i] = u[j]
-        xs_[j, 2*i + 1] = 1. - u[j]
-(Xs, Xs_) = (np.empty(n) for _ in range(2))
-for j in range(n):
-    v = 1.
-    w = 1.
-    for m in range(q):
-        v *= xs[m, j]
-        w *= xs_[m, j]
-    Xs[j] = v
-    Xs_[j] = w
-d_s = (1. + Xs) ** (-1.)
-a_s = (1. + Xs_) ** (-1.)
+xs = np.random.uniform(0, 1, (n, q))
+xs_ = xs.copy()
+xs_[1::2] = 1. - xs[::2]
+Xs = np.prod(xs, axis=-1)
+Xs_ = np.prod(xs_, axis=-1)
+d_s = 1. / (1. + Xs)
+a_s = 1. / (1. + Xs_)
 
-mu = 1. + 2. ** (-q)
-var = variance(Xs)
-cov = mean((1. + Xs) * d_s) - mu * mean(d_s)
+mu = 1. + .5 ** q
+var = 1./(3.**q) - 1./(2.**(2.*q))
+cov = np.mean((1. + Xs) * d_s) - mu * np.mean(d_s)
 cstar = -cov / var
 c_s = d_s + cstar * ((1. + Xs) - mu)
 
-average_ds1 = average_list(d_s)
-average_as1 = average_list(a_s)
-average_cs1 = average_list(c_s)
-for k in range(K):
-    average_ds[k] = average_ds1[2**(k+1)-1]
-    average_as[k] = average_as1[2**(k+1)-1]
-    average_cs[k] = average_cs1[2**(k+1)-1]
+points = np.arange(2, n+1, 2)
+average_ds = np.cumsum(d_s)[1::2] / points
+average_as = np.cumsum(a_s)[1::2] / points
+average_cs = np.cumsum(c_s)[1::2] / points
 
-plt.plot(factors, average_ds, color="r", label="Direct")
-plt.plot(factors, average_as, color="g", label="Antithetic")
-plt.plot(factors, average_cs, color="b", label="Control")
+plt.axhline(y=73*np.pi**10. / 6842880., color="k", linestyle="dashed")
+plt.plot(points, average_ds, color="r", label="Direct")
+plt.plot(points, average_as, color="g", label="Antithetic")
+plt.plot(points, average_cs, color="b", label="Control")
 plt.xscale("log", base=2)
-plt.xlim(2., n)
+plt.xlim(2, n)
 plt.xlabel(r"$n$")
 plt.ylabel(r"$\bar{f}$")
 plt.title(r"$\int_0^1\ldots\int_0^1 \frac{1}{1+x_1\ldots x_{10}}\,dx_1\ldots dx_{10}$")
